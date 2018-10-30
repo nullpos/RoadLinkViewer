fa5828dde2248ccc87f9db770e0e4a568a5ed52e
y-kisse <15102288+y-kisse@users.noreply.github.com>
2018-10-30 21:18:12 +0900

    [add] linkEdgeFetcher logic

--- utils/linkFetcher.js
+++ utils/linkFetcher.js
@@ -14,6 +14,25 @@ function makeLinkFetchQuery(latStart, lngStart, latEnd, lngEnd) {
   return query;
 }
 
+function makeLinkEdgeFetchQuery(latStart, lngStart, latEnd, lngEnd) {
+
+  let query = '';
+  query += 'SELECT  LINKS1.LINK_ID AS LINK_ID, ';
+	query += '        LINKS1.NUM AS CONSTITUTION_LINK_ID, ';
+	query	+= '        LINKS1.LATITUDE AS LATITUDE1, ';
+	query	+= '        LINKS1.LONGITUDE AS LOGITUDE1, ';
+	query += '        LINKS2.LATITUDE AS LATITUDE2, ';
+	query += '        LINKS2.LONGITUDE AS LONGITUDE2 ';
+  query += 'FROM    LINKS_GSI20 AS LINKS1 ';
+  query += '  INNER JOIN LINKS_GSI20 AS LINKS2 ON LINKS1.LINK_ID = LINKS2.LINK_ID ';
+  query += 'WHERE   LINKS1.NUM - LINKS2.NUM = 1 ';
+  query += '  AND   LINKS1.NUM > LINKS2.NUM  ';
+  query += '  AND   LINKS1.LATITUDE BETWEEN ' + parseFloat(latStart) + ' AND ' + parseFloat(latEnd) + ' ';
+  query += '  AND   LINKS1.LONGITUDE BETWEEN ' + parseFloat(lngStart) + ' AND ' + parseFloat(lngEnd) + ' ';
+  console.log(query);
+  return query;
+}
+
 exports.fetchLinksGSI20 = function(latStart, lngStart, latEnd, lngEnd, callback) {
   let connection = new Connection(config);
   let links = new Array();
@@ -25,6 +44,39 @@ exports.fetchLinksGSI20 = function(latStart, lngStart, latEnd, lngEnd, callback)
     }
 
     request = new Request(makeLinkFetchQuery(latStart, lngStart, latEnd, lngEnd), function(err, rowCount) {
+      if (err) {
+        console.log(err);
+      }
+
+      connection.close();
+      callback(links);
+    });
+
+    request.on('row', function (columns) {
+      let record = new Object();
+
+      columns.forEach(function (column) {
+        record[column.metadata.colName] = column.value;
+      });
+
+      links.push(record);
+    });
+
+    connection.execSql(request);
+  });
+}
+
+exports.fetchLinksEdgeGSI20 = function(latStart, lngStart, latEnd, lngEnd, callback) {
+  let connection = new Connection(config);
+  let links = new Array();
+
+  connection.on('connect', function(err) {
+    if (err) {
+      console.log(err);
+      return false;
+    }
+
+    request = new Request(makeLinkEdgeFetchQuery(latStart, lngStart, latEnd, lngEnd), function(err, rowCount) {
       if (err) {
         console.log(err);
       }
