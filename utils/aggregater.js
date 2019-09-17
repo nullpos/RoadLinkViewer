72ad61ca2a71139228c5a7e83ade86d65b7c88e8
y-kisse <yudaikichise@gmail.com>
2019-09-17 18:25:39 +0900

    [update] aggregator query template.

--- utils/aggregater.js
+++ utils/aggregater.js
@@ -13,7 +13,8 @@ function makeCHORALEQuery(semanticid, direction, option) {
   query += '        SUM(e.ENERGY_BY_AIR_RESISTANCE) AS air_loss, ';
   query += '        SUM(e.ENERGY_BY_ROLLING_RESISTANCE) AS rolling_loss, ';
   query += '        SUM(ABS(e.REGENE_LOSS)) AS regene_loss, ';
-  query += '        SUM(ABS(e.REGENE_ENERGY)) AS regene_energy ';
+  query += '        SUM(ABS(e.REGENE_ENERGY)) AS regene_energy, ';
+  query += '        DATEPART(hour, MIN(t.START_TIME)) AS start_hour ';
   query += 'FROM    ECOLOG_Doppler AS e ';
   query += '  INNER JOIN SEMANTIC_LINKS AS sl ON e.LINK_ID = sl.LINK_ID ';
   query += '  INNER JOIN TRIPS_Doppler AS t ON e.TRIP_ID = t.TRIP_ID ';
